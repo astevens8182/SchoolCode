bfe2715fd3cde61efaa6c74bdefda29bed977f89
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-12-09 10:43:08 -0500

    Add files via upload

---
+++ ASP.net-C#/AccessingDBTable/AccesingDBTable/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AccesingDBTable.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Database Payroll</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="Data Source=SQLEXPRESS;Initial Catalog=steveale_db;Integrated Security=True" ProviderName="System.Data.SqlClient" SelectCommand="SELECT * FROM [Payroll]"></asp:SqlDataSource>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.net-C#/AccessingDBTable/AccesingDBTable/Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccesingDBTable
+{
+
+    public partial class Default : System.Web.UI.Page {
+        decimal hoursWorked = 0.0M;
+        decimal grossPay = 0.0M;
+        decimal payRate = 0.0M;
+        double overtimeRate = 1.5;
+       
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            string query1 = "SELECT * FROM [Payroll] ";
+            string countRecords = "SELECT COUNT(*) FROM [Payroll]";
+            SqlConnection Conn1 = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["steveale_dbConnectionString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query1, Conn1);
+            SqlCommand totalCount = new SqlCommand(countRecords, Conn1);
+            Conn1.Open();
+            DataTable dataTable1 = new DataTable();
+            dataTable1.Load(cmd.ExecuteReader());
+            foreach (DataRow record1 in dataTable1.Rows)
+            //you now have a collection you can go through - in variable record1 
+            // rows is indexable 
+            // record1 is just my name for the returned rows of the result 
+            {
+                payRate = Convert.ToDecimal(record1[2]);
+                hoursWorked = Convert.ToDecimal(record1[1]);
+
+                if (hoursWorked <= 40)
+                {
+                    grossPay = payRate * hoursWorked;
+                }
+                else
+                {
+                    grossPay = 40 * payRate;
+                    grossPay += (hoursWorked - 40) * (payRate * (Convert.ToDecimal(overtimeRate)));
+                }
+                DropDownList1.Items.Add(record1[0].ToString() + " " + "Gross pay: " + string.Format("${0:#.00}", Convert.ToDecimal(grossPay)));
+                grossPay = 0;
+            }
+            DropDownList1.Items.Add("Total records: " + totalCount.ExecuteScalar().ToString());
+
+        }
+
+    }
+}
