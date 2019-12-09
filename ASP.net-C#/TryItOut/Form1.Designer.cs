7173df4bedcb8b4f59ce9c226c1bb35ef0c5dd0b
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-12-09 10:55:06 -0500

    Add files via upload

---
+++ ASP.net-C#/TryItOut/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TryItOut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.txtPayRate = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hours worked:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pay rate:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(96, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            this.txtName.Tag = "Name";
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(96, 32);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(100, 20);
+            this.txtHoursWorked.TabIndex = 4;
+            this.txtHoursWorked.Tag = "Hours worked";
+            // 
+            // txtPayRate
+            // 
+            this.txtPayRate.Location = new System.Drawing.Point(96, 56);
+            this.txtPayRate.Name = "txtPayRate";
+            this.txtPayRate.Size = new System.Drawing.Size(100, 20);
+            this.txtPayRate.TabIndex = 5;
+            this.txtPayRate.Tag = "Pay rate";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(96, 83);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 121);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtPayRate);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pay";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.TextBox txtPayRate;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ ASP.net-C#/TryItOut/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TryItOut
+{
+    public partial class Form1 : Form
+    {
+        decimal finalPay = 0;
+        decimal convertedPayRate = 0;
+        decimal convertedHoursWorked = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+                   
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (Validator.IsPresent(txtName) & Validator.IsPresent(txtHoursWorked) & Validator.IsPresent(txtPayRate) )
+            {
+                if (Validator.IsDecimal(txtHoursWorked) & Validator.IsDecimal(txtPayRate) )
+                {
+                    if (Validator.IsWithinRange(txtHoursWorked, 0, 60) & Validator.IsWithinRange(txtPayRate, 0, 999))
+                    {
+
+                        convertedHoursWorked = Convert.ToDecimal(txtHoursWorked.Text);
+                        convertedPayRate = Convert.ToDecimal(txtPayRate.Text);
+
+                        finalPay = convertedPayRate * convertedHoursWorked;
+
+                        MessageBox.Show(txtName.Text + " final pay: " + finalPay);
+                    }
+
+
+                }
+
+
+            }
+
+
+            
+        }
+    }
+}
