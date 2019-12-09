ea1569e398afe1edbbfb901c8a75009fae91b677
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-12-09 10:50:41 -0500

    Add files via upload

---
+++ ASP.net-C#/GridvViewLab/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GridViewLab.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+ 
+    <form id="form1" runat="server">
+        
+        <div>
+            <asp:GridView ID="Grid1" runat="server" AutoGenerateColumns="False" ShowHeader="False">
+                <Columns>
+                    <asp:BoundField DataField="Text" HeaderText="Name" />
+                    <asp:ImageField DataImageUrlField="Value" ControlStyle-Width="250px" ControlStyle-Height="250px" HeaderText="Images" />
+                </Columns>
+            </asp:GridView>
+            <asp:FileUpload ID="fileUploadControl" runat="server" />
+            <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.net-C#/GridvViewLab/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace GridViewLab
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] stringArray = Directory.GetFiles(Server.MapPath("~/Images/"));
+            List<ListItem> Imgs = new List<ListItem>();
+            foreach (string l in stringArray)
+            {
+               string shortname  = Path.GetFileName(l);
+
+                string imageName = "~/Images/" + shortname ;
+                Imgs.Add(new ListItem(shortname, imageName));
+                Grid1.DataSource = Imgs;
+                Grid1.DataBind();
+            }
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+
+            if (fileUploadControl.HasFile)
+            {
+              string  fileName = fileUploadControl.FileName;
+               fileUploadControl.PostedFile.SaveAs(Server.MapPath("~/Images/") + fileName);
+                Response.Redirect(Request.Url.AbsoluteUri);
+            }
+        }
+    }
+}
