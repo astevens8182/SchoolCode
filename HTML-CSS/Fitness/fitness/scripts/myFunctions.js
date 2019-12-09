e8fee45c15dcfe8315c21940d26604097aae1579
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-12-09 11:07:59 -0500

    Add files via upload

---
+++ HTML-CSS/Fitness/fitness/scripts/myFunctions.js
@@ -0,0 +1,21 @@
+/* Alex Stevens
+4/2/2018
+My Function list
+*/
+
+
+
+//Burpees video window
+function burpees() {
+  window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+}
+
+//Plank Video window
+function plank() {
+  window.open("scripts/plank.html", "_blank", "width=610, height=360");
+}
+
+//Mountain climber video window
+function mtnClimber() {
+  window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+}
