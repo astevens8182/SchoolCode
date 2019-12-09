74c5cc60ec90c6ad5970c35d958d1a1e561ef38f
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-12-09 11:23:17 -0500

    Add files via upload

---
+++ JavaScript-Vue-Firebase/FirebaseClass/script.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCBfuN1w6kP-Vq98Ce1bfIfx79Qj3KSqeo",
+    authDomain: "cis-371-friday.firebaseapp.com",
+    databaseURL: "https://cis-371-friday.firebaseio.com",
+    projectId: "cis-371-friday",
+    storageBucket: "",
+    messagingSenderId: "57750652889",
+    appId: "1:57750652889:web:3684c1dedb354b1655bbc8",
+    measurementId: "G-WM2F7S9K0R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
