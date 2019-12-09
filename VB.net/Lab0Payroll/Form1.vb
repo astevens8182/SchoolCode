1866c1fcfeae97ea8388b84a108c38078ae5d0eb
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-12-09 10:13:05 -0500

    Add files via upload

---
+++ VB.net/Lab0Payroll/Form1.vb
@@ -0,0 +1,18 @@
+﻿'Author: Alex Stevens'
+'Date 1/9/2018'
+'Assigment: Lab 0'
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        TxtPay.Text = TxtHoursWorked.Text * TxtPayRate.Text
+
+    End Sub
+End Class
