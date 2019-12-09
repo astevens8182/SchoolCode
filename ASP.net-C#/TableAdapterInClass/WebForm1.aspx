79c40401b0ece2c1bd4d7a8ba7a0c02cbbf7dc89
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-12-09 10:54:31 -0500

    Add files via upload

---
+++ ASP.net-C#/TableAdapterInClass/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TableAdapterInClass.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtFirst" runat="server"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtLast" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+            <br />
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.net-C#/TableAdapterInClass/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TableAdapterInClass
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.AddressesTableAdapter tableAdapter = new DataSet1TableAdapters.AddressesTableAdapter();
+            tableAdapter.InsertQuery(txtFirst.Text, txtLast.Text);
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.AddressesTableAdapter tableAdapter = new DataSet1TableAdapters.AddressesTableAdapter();
+            tableAdapter.DeleteQuery(txtFirst.Text, txtLast.Text);
+        }
+    }
+}
